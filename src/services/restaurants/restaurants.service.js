d9978bfb8e09eba374dd22bbf8ce0115c083277c
ThalesBMC <thalesbmc@gmail.com>
2021-04-28 15:37:16 -0300

    promises for my data

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,7 +1,31 @@
 import { mocks } from "./mock";
-
-export const restaurantsRequest = (location = "41.878113,-87.629799") => {
-  console.log(mocks[location]);
+import camelize from "camelize";
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+    if (!mock) {
+      reject("not found");
+    }
+    resolve(mock);
+  });
 };
 
-restaurantsRequest();
+const restaurantTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
+  });
+
+  return camelize(mappedResults);
+};
+restaurantsRequest()
+  .then(restaurantTransform)
+  .then((transformed) => {
+    console.log(transformed);
+  })
+  .catch((err) => {
+    console.log("error");
+  });
